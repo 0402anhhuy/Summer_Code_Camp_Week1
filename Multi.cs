1992f9359eb0a5f0c0777a4863fb98dcef964f63
Trần Anh Huy <anhhuyrubic@gmail.com>
2024-07-11 16:43:20 +0700

    Update: Multi-func

---
+++ Multi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+namespace MyConsoleApp{
+    class Program{
+        static int Multi(int a,int b){
+            return a*b;
+        }
+
+         static void Main(string[] args){
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int multi = Multi(a, b);
+            Console.WriteLine($"a + b = {multi}");
+        }
+    }
+}
