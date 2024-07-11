9014fe16877eccf351f4e872415b8f47e0ffe239
DavidDet <quocdat7022005@gmail.com>
2024-07-11 18:21:05 +0700

    Update mul second

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace MyConsoleApp{
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int multi = Multi(a, b);
+            Console.WriteLine("Ket qua của ");
             Console.WriteLine($"a + b = {multi}");
         }
     }
