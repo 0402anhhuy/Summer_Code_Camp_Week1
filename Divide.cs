e3a49adbc4345572b35a57dcb975c98a33fdfa7b
Trần Anh Huy <anhhuyrubic@gmail.com>
2024-07-11 16:13:06 +0700

    Update: Add div-func

---
+++ Divide.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Net.Security;
+namespace MyConsoleApp{
+    public class Divide{
+        public static double divide(int a, int b){
+            return (double)a / b;
+        }
+    }
+}
