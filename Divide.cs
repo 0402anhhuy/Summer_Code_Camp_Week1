7e657ce58cccf84efee31c45818f91baf181ef8e
Trần Anh Huy <142443076+0402anhhuy@users.noreply.github.com>
2024-07-11 17:12:18 +0700

    Delete Divide.cs

--- Divide.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Net.Security;
-namespace MyConsoleApp{
-    public class Divide{
-        public static double divide(int a, int b){
-            return (double)a / b;
-        }
-    }
-}
