f08a97d0de7e3fcb45914b244f85469c3bbccb82
VinhThuy-2403 <lvthuy2403@gmail.com>
2024-07-11 18:40:51 +0700

    Update: Add Function

---
+++ Add.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Net.Security;
+namespace MyConsoleApp{
+    public class Add{
+        public static double Add(int a, int b){
+            return a+b;
+        }
+    }
+}
